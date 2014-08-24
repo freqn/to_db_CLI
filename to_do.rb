482354a6713e6ff0e8d12d044a8588d6faf55422
c <freqnlabs@gmail.com>
2014-08-24 17:45:35 -0400

    Added ability to add tasks

--- to_do.rb
+++ to_do.rb
@@ -11,12 +11,14 @@ end
 
 def main_menu
   spc
-  puts "Type 'c' to create a list, 's' to show your lists or 'd' to delete a list"
+  puts "Type 'c' to create a list, 't' to create a task, 's' to show your lists or 'd' to delete a list"
   spc
   menu_sel = gets.chomp.downcase
   case menu_sel
   when 'c'
     create_list
+  when 't'
+    create_task
   when 's'
     show_lists
   when 'd'
@@ -47,9 +49,42 @@ def show_lists
   spc
   results = DB.exec("SELECT * FROM lists;")
   results.each do |result|
-    puts "List #{result['id']}: #{result['name']}"
+    puts "List #{result['id']}: #{result['name']}" 
   end
   main_menu
+end
+
+def create_task
+  spc
+  puts "You have the following lists:"
+  spc
+  list_ids = []
+  results = DB.exec("SELECT * FROM lists;")
+  results.each do |result|
+    puts "List #{result['id']}: #{result['name']}"
+    list_ids << result['id']
+  end
+  spc
+  puts "Type the number of the list you'd like to use or 'c' to create a new one"
+  spc
+  create_sel = gets.chomp.downcase
+  if list_ids.include?(create_sel)
+    id = create_sel
+    spc
+    puts "Type a name for your task"
+    spc
+    task_name = gets.chomp.split.map(&:capitalize).join(' ')
+    task = Task.new(task_name, id)
+    task.save
+    spc
+    puts "Task '#{task_name}' has been created"
+  elsif create_sel == 'c'
+    create_list
+  else
+    puts "Follow the instructions next time. Returning to the Main menu"
+  end
+  main_menu
+
 end
 
 
